8c9cf32c4c234ea1cdfd17d2057b7d278887c371
NathanQid <114936633+NathanQid@users.noreply.github.com>
2022-10-04 00:33:39 +0000

    Add files via upload

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+   
+
+
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+             
+    }
+
+    // Update is called once per frame
+    private float propellorSpeed = 1000;
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, propellorSpeed * Time.deltaTime);
+    }
+}
